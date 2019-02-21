66a88c2b3e4d13b912f825327dc95b4422103972
tiyb <thinkinginsideyourbox@gmail.com>
2019-02-21 11:29:46 -0500

    minor formatting changes

--- src/main/resources/static/js/conversations.js
+++ src/main/resources/static/js/conversations.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		
 		data.forEach(element => words.push({text: element.participant, weight: element.numMessages, handlers: {click: function(item) {window.open("/conversationViewer?participant=" + item.target.textContent, "viewer", "menubar=no,status=no,toolbar=no,height=700,width=1000");}}}));
 		
-		$('#conversationWordCloudContainer').jQCloud(words, {width:700,height:500,fontSize:{from:0.1,to:0.02},autoResize:true});
+		$('#conversationWordCloudContainer').jQCloud(words, {width:900,height:500,fontSize:{from:0.1,to:0.02},autoResize:true});
 	});
 	
 });
