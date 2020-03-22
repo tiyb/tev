b2548cb1f94da5030ee21a84d498b3d3bdb71344
tiyb <thinkinginsideyourbox@gmail.com>
2020-03-22 15:04:12 -0400

    changed the way sample files are read in, to force reading in UTF-8

--- src/test/java/com/tiyb/tev/xml/ExportWritingUnitTests.java
+++ src/test/java/com/tiyb/tev/xml/ExportWritingUnitTests.java
@@ -7,10 +7,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,90 +33,92 @@ import com.tiyb.tev.datamodel.Metadata;
 @AutoConfigureMockMvc
 public class ExportWritingUnitTests {
 
-	@Autowired
-	private TEVPostRestController postController;
+    @Autowired
+    private TEVPostRestController postController;
 
-	@Autowired
-	private TEVMetadataRestController mdController;
+    @Autowired
+    private TEVMetadataRestController mdController;
 
-	@Value("classpath:XML/outputs/response-xml-answer.xml")
-	Resource answerXML;
+    @Value("classpath:XML/outputs/response-xml-answer.xml")
+    Resource answerXML;
 
-	@Value("classpath:XML/outputs/response-xml-regular.xml")
-	Resource regularXML;
+    @Value("classpath:XML/outputs/response-xml-regular.xml")
+    Resource regularXML;
 
-	@Value("classpath:XML/outputs/response-xml-link.xml")
-	Resource linkXML;
+    @Value("classpath:XML/outputs/response-xml-link.xml")
+    Resource linkXML;
 
-	@Value("classpath:XML/outputs/response-xml-singlephoto.xml")
-	Resource singlePhotoXML;
+    @Value("classpath:XML/outputs/response-xml-singlephoto.xml")
+    Resource singlePhotoXML;
 
-	@Value("classpath:XML/outputs/response-xml-multiplephotos.xml")
-	Resource multiplePhotoXML;
+    @Value("classpath:XML/outputs/response-xml-multiplephotos.xml")
+    Resource multiplePhotoXML;
 
-	private static String ANSWER_POST_ID = "180371366195";
-	private static String REGULAR_POST_ID = "180894436671";
-	private static String LINK_POST_ID = "180265557725";
-	private static String SINGLEPHOTO_POST_ID = "180784644740";
-	private static String MULTIPLEPHOTO_POST_ID = "180254465582";
+    private static String ANSWER_POST_ID = "180371366195";
+    private static String REGULAR_POST_ID = "180894436671";
+    private static String LINK_POST_ID = "180265557725";
+    private static String SINGLEPHOTO_POST_ID = "180784644740";
+    private static String MULTIPLEPHOTO_POST_ID = "180254465582";
 
-	private static String MAIN_BLOG_NAME = "mainblog";
+    private static String MAIN_BLOG_NAME = "mainblog";
 
-	@Before
-	public void setupData() throws FileNotFoundException {
-		File rawXmlFile = ResourceUtils.getFile("classpath:XML/test-post-xml.xml");
-		InputStream xmlFile = new FileInputStream(rawXmlFile);
+    @Before
+    public void setupData() throws FileNotFoundException {
+        File rawXmlFile = ResourceUtils.getFile("classpath:XML/test-post-xml.xml");
+        InputStream xmlFile = new FileInputStream(rawXmlFile);
 
-		Metadata md = mdController.getMetadataForBlogOrDefault(MAIN_BLOG_NAME);
-		md.setOverwritePostData(true);
-		md.setIsDefault(true);
-		md.setBlog(MAIN_BLOG_NAME);
-		md = mdController.updateMetadata(md.getId(), md);
+        Metadata md = mdController.getMetadataForBlogOrDefault(MAIN_BLOG_NAME);
+        md.setOverwritePostData(true);
+        md.setIsDefault(true);
+        md.setBlog(MAIN_BLOG_NAME);
+        md = mdController.updateMetadata(md.getId(), md);
 
-		BlogXmlReader.parseDocument(xmlFile, postController, MAIN_BLOG_NAME);
-	}
+        BlogXmlReader.parseDocument(xmlFile, postController, MAIN_BLOG_NAME);
+    }
 
-	@Test
-	public void testExportOfRegular() throws IOException {
-		testSinglePostResponse(regularXML, REGULAR_POST_ID);
-	}
+    @Test
+    public void testExportOfRegular() throws IOException {
+        testSinglePostResponse(regularXML, REGULAR_POST_ID);
+    }
 
-	@Test
-	public void testExportOfAnswer() throws IOException {
-		testSinglePostResponse(answerXML, ANSWER_POST_ID);
-	}
+    @Test
+    public void testExportOfAnswer() throws IOException {
+        testSinglePostResponse(answerXML, ANSWER_POST_ID);
+    }
 
-	@Test
-	public void testExportOfLink() throws IOException {
-		testSinglePostResponse(linkXML, LINK_POST_ID);
-	}
+    @Test
+    public void testExportOfLink() throws IOException {
+        testSinglePostResponse(linkXML, LINK_POST_ID);
+    }
 
-	@Test
-	public void testExportOfPhoto() throws IOException {
-		testSinglePostResponse(singlePhotoXML, SINGLEPHOTO_POST_ID);
-	}
+    @Test
+    public void testExportOfPhoto() throws IOException {
+        testSinglePostResponse(singlePhotoXML, SINGLEPHOTO_POST_ID);
+    }
 
-	@Test
-	public void testExportOfPhotoMultiple() throws IOException {
-		testSinglePostResponse(this.multiplePhotoXML, MULTIPLEPHOTO_POST_ID);
-	}
+    @Test
+    public void testExportOfPhotoMultiple() throws IOException {
+        testSinglePostResponse(this.multiplePhotoXML, MULTIPLEPHOTO_POST_ID);
+    }
 
-	private String getExpectedResponse(Resource resource) throws IOException {
-		File expectedResponseFile = resource.getFile();
-		String expectedResponseString = new String(Files.readAllBytes(expectedResponseFile.toPath()));
+    private String getExpectedResponse(Resource resource) throws IOException {
+        File expectedResponseFile = resource.getFile();
+        List<String> expectedResponseStrings =
+                Files.readAllLines(expectedResponseFile.toPath(), StandardCharsets.UTF_8);
+        String expectedResponseString = String.join(StringUtils.EMPTY, expectedResponseStrings);
 
-		return expectedResponseString;
-	}
+        return expectedResponseString;
+    }
 
-	private void testSinglePostResponse(Resource resource, String postID) throws IOException {
-		String expectedAnswer = getExpectedResponse(resource);
+    private void testSinglePostResponse(Resource resource, String postID) throws IOException {
+        String expectedAnswer = getExpectedResponse(resource);
 
-		List<String> postIDs = new ArrayList<String>();
-		postIDs.add(postID);
+        List<String> postIDs = new ArrayList<String>();
+        postIDs.add(postID);
 
-		String result = BlogXmlWriter.getStagedPostXMLForBlog(postIDs, postController, MAIN_BLOG_NAME);
+        String result = BlogXmlWriter.getStagedPostXMLForBlog(postIDs, postController, MAIN_BLOG_NAME);
 
-		assertThat(result).isEqualToIgnoringWhitespace(expectedAnswer);
-	}
+        assertThat(result).isEqualToIgnoringWhitespace(expectedAnswer);
+    }
 
 }
