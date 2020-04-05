82ee2555e7d10fb4dd3db80d744b638be52d74f9
tiyb <thinkinginsideyourbox@gmail.com>
2020-04-05 14:47:52 -0400

    simplified logic greatly

--- src/main/resources/static/js/taghandler.js
+++ src/main/resources/static/js/taghandler.js
@@ -3,21 +3,14 @@
  * re-navigate the parent to the posts page, searching for this hashtag (if
  * there <i>is</i> a parent, or navigate the current page to the same.
  */
-$(document).ready(
-		function() {
-			$(document).on(
-					'click',
-					'span[class=hashtagspan]',
-					function() {
-						var newHostURL = window.location.protocol + "//"
-								+ window.location.hostname + ":"
-								+ window.location.port + "/?hashsearch="
-								+ encodeURI($(this).text());
-						if (window.opener == null) {
-							location.replace(newHostURL);
-						} else {
-							window.parent.opener.location.replace(newHostURL);
-							window.close();
-						}
-					});
-		});
+$(document).ready(function() {
+	$(document).on('click', 'span[class=hashtagspan]', function() {
+		var newHostURL = "/index?hashsearch=" + encodeURI($(this).text());
+		if (window.opener == null) {
+			location.replace(newHostURL);
+		} else {
+			window.parent.opener.location.replace(newHostURL);
+			window.close();
+		}
+	});
+});
