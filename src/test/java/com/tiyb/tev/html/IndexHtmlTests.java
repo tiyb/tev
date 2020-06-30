f32e8c8d0160adc6d7fc9cf40a8e7b8a01bf8487
tiyb <thinkinginsideyourbox@gmail.com>
2020-06-30 19:11:13 -0400

    began work of setting up MD

--- src/test/java/com/tiyb/tev/html/IndexHtmlTests.java
+++ src/test/java/com/tiyb/tev/html/IndexHtmlTests.java
@@ -9,14 +9,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.tiyb.tev.TevTestingHelpers;
+import com.tiyb.tev.datamodel.Metadata;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -24,14 +30,28 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 public class IndexHtmlTests {
 
     private WebClient webClient;
-    
+
     @LocalServerPort
     private int serverPort;
-    
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private Logger logger = LoggerFactory.getLogger(IndexHtmlTests.class);
+
     @Before
     public void setupWC() {
         webClient = new WebClient();
         webClient.getOptions().setThrowExceptionOnScriptError(false);
+
+        Metadata md = restTemplate.getForObject("http://localhost:" + this.serverPort + "/api/metadata/byBlog/"
+                + TevTestingHelpers.MAIN_BLOG_NAME + "/orDefault", Metadata.class);
+        md.setOverwritePostData(true);
+        md.setOverwriteConvoData(true);
+        md.setMainTumblrUser(TevTestingHelpers.MAIN_BLOG_NAME);
+        md.setIsDefault(true);
+        restTemplate.put("http://localhost:" + this.serverPort + "/api/metadata/" + md.getId(), md);
+        md = restTemplate.getForObject("http://localhost:" + this.serverPort + "/api/metadata/byBlog/"
+                + TevTestingHelpers.MAIN_BLOG_NAME + "/orDefault", Metadata.class);
     }
 
     @After
