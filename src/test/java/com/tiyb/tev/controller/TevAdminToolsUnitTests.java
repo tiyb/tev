98405d8afd26a9e1df553b5bebf651f64094abe3
tiyb <thinkinginsideyourbox@gmail.com>
2021-01-19 15:35:04 -0500

    added direct calls to controller back in

--- src/test/java/com/tiyb/tev/controller/TevAdminToolsUnitTests.java
+++ src/test/java/com/tiyb/tev/controller/TevAdminToolsUnitTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -24,6 +25,10 @@ import com.tiyb.tev.html.HtmlTestingClass;
  */
 public class TevAdminToolsUnitTests extends HtmlTestingClass {
 
+    @Autowired
+    private TEVAdminToolsController adminRestController;
+    @Autowired
+    private TEVPostRestController postController;
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -60,6 +65,21 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
     public void cleanExtraImages() throws IOException {
         tempMDImageFolder.newFile("blah.txt");
 
+        adminRestController.cleanImagesOnHDForBlog(MAIN_BLOG_NAME);
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(0);
+    }
+
+    /**
+     * Tests that any files not associated with a post are deleted from the folder,
+     * via the REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void cleanExtraImagesRest() throws IOException {
+        tempMDImageFolder.newFile("blah.txt");
+
         restTemplate.getForEntity(String.format("%s/admintools/posts/%s/cleanImagesOnHD", baseUri(), MAIN_BLOG_NAME),
                 String.class);
 
@@ -74,6 +94,23 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
      */
     @Test
     public void cleanExactNumImages() throws IOException {
+        tempMDImageFolder.newFile("180784644740_0.gif");
+        tempMDImageFolder.newFile("180254465582_0.gif");
+        tempMDImageFolder.newFile("180254465582_1.gif");
+
+        adminRestController.cleanImagesOnHDForBlog(MAIN_BLOG_NAME);
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(3);
+    }
+
+    /**
+     * Tests that nothing is deleted when the exact number of files is in the folder
+     * as expected, via the REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void cleanExactNumImagesRest() throws IOException {
         tempMDImageFolder.newFile("180784644740_0.gif");
         tempMDImageFolder.newFile("180254465582_0.gif");
         tempMDImageFolder.newFile("180254465582_1.gif");
@@ -99,6 +136,26 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
         tempMDImageFolder.newFile("180254465582_2.gif");
         tempMDImageFolder.newFile("180254465582_3.gif");
 
+        adminRestController.cleanImagesOnHDForBlog(MAIN_BLOG_NAME);
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(3);
+    }
+
+    /**
+     * Tests that duplicate images are removed from the folder (the core result of
+     * the feature), via the REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void cleanDoubleImagesRest() throws IOException {
+        tempMDImageFolder.newFile("180784644740_0.gif");
+        tempMDImageFolder.newFile("180784644740_1.gif");
+        tempMDImageFolder.newFile("180254465582_0.gif");
+        tempMDImageFolder.newFile("180254465582_1.gif");
+        tempMDImageFolder.newFile("180254465582_2.gif");
+        tempMDImageFolder.newFile("180254465582_3.gif");
+
         restTemplate.getForEntity(String.format("%s/admintools/posts/%s/cleanImagesOnHD", baseUri(), MAIN_BLOG_NAME),
                 String.class);
 
@@ -112,6 +169,20 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
      */
     @Test
     public void importExtraImages() throws IOException {
+        tempInputImageFolder.newFile("blah.txt");
+
+        adminRestController.importImagesForBlog(MAIN_BLOG_NAME, tempInputImageFolder.getRoot().getAbsolutePath());
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(0);
+    }
+
+    /**
+     * Test importing images with non-post-related images, via the REST intrface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void importExtraImagesRest() throws IOException {
         tempInputImageFolder.newFile("blah.txt");
 
         restTemplate.postForObject(String.format("%s/admintools/posts/%s/importImages", baseUri(), MAIN_BLOG_NAME),
@@ -131,6 +202,22 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
         tempInputImageFolder.newFile("180254465582_0.gif");
         tempInputImageFolder.newFile("180254465582_1.gif");
 
+        adminRestController.importImagesForBlog(MAIN_BLOG_NAME, tempInputImageFolder.getRoot().getAbsolutePath());
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(3);
+    }
+
+    /**
+     * Test import where the right number of images exists, via the REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void importExactNumImagesRest() throws IOException {
+        tempInputImageFolder.newFile("180784644740_0.gif");
+        tempInputImageFolder.newFile("180254465582_0.gif");
+        tempInputImageFolder.newFile("180254465582_1.gif");
+
         restTemplate.postForObject(String.format("%s/admintools/posts/%s/importImages", baseUri(), MAIN_BLOG_NAME),
                 tempInputImageFolder.getRoot().getAbsolutePath(), String.class);
 
@@ -144,6 +231,25 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
      */
     @Test
     public void importDoubleImages() throws IOException {
+        tempInputImageFolder.newFile("180784644740_0.gif");
+        tempInputImageFolder.newFile("180784644740_1.gif");
+        tempInputImageFolder.newFile("180254465582_0.gif");
+        tempInputImageFolder.newFile("180254465582_1.gif");
+        tempInputImageFolder.newFile("180254465582_2.gif");
+        tempInputImageFolder.newFile("180254465582_3.gif");
+
+        adminRestController.importImagesForBlog(MAIN_BLOG_NAME, tempInputImageFolder.getRoot().getAbsolutePath());
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(3);
+    }
+
+    /**
+     * Test import where the input folder has duplicates, via the REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void importDoubleImagesRest() throws IOException {
         tempInputImageFolder.newFile("180784644740_0.gif");
         tempInputImageFolder.newFile("180784644740_1.gif");
         tempInputImageFolder.newFile("180254465582_0.gif");
@@ -171,6 +277,26 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
         tempInputImageFolder.newFile("180254465582_2.gif");
         tempInputImageFolder.newFile("180254465582_3.gif");
 
+        adminRestController.importImagesForBlog(MAIN_BLOG_NAME, tempInputImageFolder.getRoot().getAbsolutePath());
+
+        assertThat(tempMDImageFolder.getRoot().list().length).isEqualTo(3);
+    }
+
+    /**
+     * Test import when there are already images in the destination folder, via the
+     * REST interface
+     *
+     * @throws IOException
+     */
+    @Test
+    public void importWithExistingRest() throws IOException {
+        tempMDImageFolder.newFile("180784644740_0.gif");
+        tempMDImageFolder.newFile("180784644740_1.gif");
+        tempInputImageFolder.newFile("180254465582_0.gif");
+        tempInputImageFolder.newFile("180254465582_1.gif");
+        tempInputImageFolder.newFile("180254465582_2.gif");
+        tempInputImageFolder.newFile("180254465582_3.gif");
+
         restTemplate.postForObject(String.format("%s/admintools/posts/%s/importImages", baseUri(), MAIN_BLOG_NAME),
                 tempInputImageFolder.getRoot().getAbsolutePath(), String.class);
 
@@ -182,6 +308,34 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
      */
     @Test
     public void markAllPostsRead() {
+        List<Post> allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+
+        for (Post p : allPosts) {
+            p.setIsRead(false);
+            postController.updatePostForBlog(p.getTumblelog(), p.getId(), p);
+        }
+
+        allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+        for (Post p : allPosts) {
+            assertThat(p.getIsRead()).isEqualTo(false);
+        }
+
+        adminRestController.markAllPostsReadForBlog(MAIN_BLOG_NAME);
+
+        allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+        for (Post p : allPosts) {
+            assertThat(p.getIsRead()).isEqualTo(true);
+        }
+    }
+
+    /**
+     * Tests functionality for marking all posts read, via the REST interface
+     */
+    @Test
+    public void markAllPostsReadRest() {
         Post[] allPosts = getAllPostsFromRest(MAIN_BLOG_NAME);
         assertThat(allPosts).isNotEmpty();
 
@@ -211,6 +365,34 @@ public class TevAdminToolsUnitTests extends HtmlTestingClass {
      */
     @Test
     public void markAllPostsUnread() {
+        List<Post> allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+
+        for (Post p : allPosts) {
+            p.setIsRead(true);
+            postController.updatePostForBlog(p.getTumblelog(), p.getId(), p);
+        }
+
+        allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+        for (Post p : allPosts) {
+            assertThat(p.getIsRead()).isEqualTo(true);
+        }
+
+        adminRestController.markAllPostsUnreadForBlog(MAIN_BLOG_NAME);
+
+        allPosts = postController.getAllPostsForBlog(MAIN_BLOG_NAME);
+        assertThat(allPosts).isNotEmpty();
+        for (Post p : allPosts) {
+            assertThat(p.getIsRead()).isEqualTo(false);
+        }
+    }
+
+    /**
+     * Tests functionality for marking all posts unread, via the REST interface
+     */
+    @Test
+    public void markAllPostsUnreadRest() {
         Post[] allPosts = getAllPostsFromRest(MAIN_BLOG_NAME);
         assertThat(allPosts).isNotEmpty();
 
