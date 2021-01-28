a7c77fe4fcb2cc61dac66d6592473e8e211077be
tiyb <thinkinginsideyourbox@gmail.com>
2021-01-28 17:33:27 -0500

    added RESTful API call tests

--- src/test/java/com/tiyb/tev/controller/TevConvoRestControllerUnitTests.java
+++ src/test/java/com/tiyb/tev/controller/TevConvoRestControllerUnitTests.java
@@ -7,10 +7,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-import com.tiyb.tev.TevTestingClass;
 import com.tiyb.tev.datamodel.Conversation;
 import com.tiyb.tev.datamodel.ConversationMessage;
+import com.tiyb.tev.html.HtmlTestingClass;
 
 /**
  * <p>
@@ -29,10 +31,12 @@ import com.tiyb.tev.datamodel.ConversationMessage;
  * same post; 5) verify all of the fields are the same
  * </p>
  */
-public class TevConvoRestControllerUnitTests extends TevTestingClass {
+public class TevConvoRestControllerUnitTests extends HtmlTestingClass {
 
     @Autowired
     private TEVConvoRestController restController;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     /**
      * Clean out conversations before each test
@@ -69,6 +73,34 @@ public class TevConvoRestControllerUnitTests extends TevTestingClass {
     }
 
     /**
+     * Verifies that updating a Conversation properly updates all fields, via REST
+     */
+    @Test
+    public void updateConversationRest() {
+        Conversation original = new Conversation();
+
+        Conversation modified = restTemplate.postForObject(
+                String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME), original, Conversation.class);
+        assertThat(modified).isNotNull();
+
+        modified.setNumMessages(5);
+        modified.setParticipant("new participant");
+        modified.setParticipantAvatarUrl("avatar URL");
+        modified.setParticipantId("pid123");
+        modified.setHideConversation(true);
+
+        restTemplate.put(String.format("%s/api/conversations/%s/%d", baseUri(), MAIN_BLOG_NAME, modified.getId()),
+                modified);
+
+        Conversation finalFromServer = restTemplate.getForObject(
+                String.format("%s/api/conversations/%s/byParticipant/%s", baseUri(), MAIN_BLOG_NAME, "new participant"),
+                Conversation.class);
+
+        assertThat(finalFromServer).isNotNull();
+        assertThat(finalFromServer).isEqualToComparingFieldByField(modified);
+    }
+
+    /**
      * Verify that marking a Conversation "hidden" in the DB really does mark it
      * hidden
      */
@@ -95,6 +127,34 @@ public class TevConvoRestControllerUnitTests extends TevTestingClass {
     }
 
     /**
+     * Verify that marking a Conversation "hidden" in the DB really does mark it
+     * hidden, via REST
+     */
+    @Test
+    public void markConversationHiddenRest() {
+        String participantName = "participant1";
+
+        Conversation original = new Conversation();
+        original.setId(1L);
+        original.setHideConversation(false);
+        original.setParticipant(participantName);
+        original.setBlog(MAIN_BLOG_NAME);
+
+        original = restTemplate.postForObject(String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME),
+                original, Conversation.class);
+        assertThat(original).isNotNull();
+
+        restTemplate.put(String.format("%s/api/conversations/%s/byParticipant/%s/ignoreConvo", baseUri(),
+                MAIN_BLOG_NAME, participantName), null);
+
+        Conversation finalConvo = restTemplate.getForObject(
+                String.format("%s/api/conversations/%s/byParticipant/%s", baseUri(), MAIN_BLOG_NAME, participantName),
+                Conversation.class);
+        assertThat(finalConvo).isNotNull();
+        assertThat(finalConvo.getHideConversation()).isEqualTo(true);
+    }
+
+    /**
      * Verify that marking a Conversation "un-hidden" in the DB really does un-hide
      * it
      */
@@ -116,6 +176,34 @@ public class TevConvoRestControllerUnitTests extends TevTestingClass {
         assertThat(newConvo.getHideConversation()).isEqualTo(false);
 
         Conversation finalConvo = restController.getConversationForBlogByParticipant(MAIN_BLOG_NAME, participantName);
+        assertThat(finalConvo).isNotNull();
+        assertThat(finalConvo.getHideConversation()).isEqualTo(false);
+    }
+
+    /**
+     * Verify that marking a Conversation "un-hidden" in the DB really does un-hide
+     * it, via REST
+     */
+    @Test
+    public void markConversationUnhiddenRest() {
+        String participantName = "participant1";
+
+        Conversation original = new Conversation();
+        original.setId(1L);
+        original.setHideConversation(true);
+        original.setParticipant(participantName);
+        original.setBlog(MAIN_BLOG_NAME);
+
+        original = restTemplate.postForObject(String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME),
+                original, Conversation.class);
+        assertThat(original).isNotNull();
+
+        restTemplate.put(String.format("%s/api/conversations/%s/byParticipant/%s/unignoreConvo", baseUri(),
+                MAIN_BLOG_NAME, participantName), null);
+
+        Conversation finalConvo = restTemplate.getForObject(
+                String.format("%s/api/conversations/%s/byParticipant/%s", baseUri(), MAIN_BLOG_NAME, participantName),
+                Conversation.class);
         assertThat(finalConvo).isNotNull();
         assertThat(finalConvo.getHideConversation()).isEqualTo(false);
     }
@@ -159,6 +247,50 @@ public class TevConvoRestControllerUnitTests extends TevTestingClass {
     }
 
     /**
+     * Same as {@link #markAllConversationsUnhidden()}, but via REST
+     */
+    @Test
+    public void markAllConversationsUnhiddenRest() {
+        String participant1 = "participant1";
+        String participant2 = "participant2";
+
+        Conversation convo1 = new Conversation();
+        convo1.setId(1L);
+        convo1.setParticipant(participant1);
+        convo1.setHideConversation(true);
+        restTemplate.postForObject(String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME), convo1,
+                Conversation.class);
+        Conversation convo2 = new Conversation();
+        convo2.setId(2L);
+        convo2.setParticipant(participant2);
+        convo2.setHideConversation(true);
+        restTemplate.postForObject(String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME), convo2,
+                Conversation.class);
+
+        ResponseEntity<Conversation[]> responseEntity = restTemplate.getForEntity(
+                String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME), Conversation[].class);
+        Conversation[] allConvos = responseEntity.getBody();
+        assertThat(allConvos).isNotEmpty();
+        assertThat(allConvos.length).isEqualTo(2);
+
+        responseEntity = restTemplate.getForEntity(
+                String.format("%s/api/conversations/%s/unhidden", baseUri(), MAIN_BLOG_NAME), Conversation[].class);
+        Conversation[] allUnHiddenConvos = responseEntity.getBody();
+        assertThat(allUnHiddenConvos).isEmpty();
+        assertThat(allUnHiddenConvos.length).isEqualTo(0);
+
+        restTemplate.getForObject(
+                String.format("%s/api/conversations/%s/unignoreAllConversations", baseUri(), MAIN_BLOG_NAME),
+                String.class);
+
+        responseEntity = restTemplate.getForEntity(
+                String.format("%s/api/conversations/%s/unhidden", baseUri(), MAIN_BLOG_NAME), Conversation[].class);
+        Conversation[] finalList = responseEntity.getBody();
+        assertThat(finalList).isNotEmpty();
+        assertThat(finalList.length).isEqualTo(2);
+    }
+
+    /**
      * Verifies that updating a Conversation Message properly updates all fields
      */
     @Test
@@ -185,6 +317,43 @@ public class TevConvoRestControllerUnitTests extends TevTestingClass {
         assertThat(finalFromServer).isNotNull();
         assertThat(finalFromServer.size()).isEqualTo(1);
         assertThat(finalFromServer.get(0)).isEqualToComparingFieldByField(modified);
+    }
+
+    /**
+     * Verifies that updating a Conversation Message properly updates all fields,
+     * via REST
+     */
+    @Test
+    public void updateConvoMsgRest() {
+        Conversation convo = new Conversation();
+        convo.setBlog(MAIN_BLOG_NAME);
+        convo = restTemplate.postForObject(String.format("%s/api/conversations/%s", baseUri(), MAIN_BLOG_NAME), convo,
+                Conversation.class);
+        ConversationMessage original = new ConversationMessage();
+        original.setConversationId(convo.getId());
+
+        ConversationMessage modified = restTemplate.postForObject(
+                String.format("%s/api/conversations/%s/messages", baseUri(), MAIN_BLOG_NAME), original,
+                ConversationMessage.class);
+        assertThat(modified).isNotNull();
+
+        modified.setMessage("message");
+        modified.setReceived(true);
+        modified.setTimestamp(25L);
+        modified.setType("IMAGE");
+
+        restTemplate.put(
+                String.format("%s/api/conversations/%s/messages/%d", baseUri(), MAIN_BLOG_NAME, modified.getId()),
+                modified);
+
+        ResponseEntity<ConversationMessage[]> responseEntity = restTemplate
+                .getForEntity(String.format("%s/api/conversations/%s/messagesForConvoId/%d/messages", baseUri(),
+                        MAIN_BLOG_NAME, convo.getId()), ConversationMessage[].class);
+        ConversationMessage[] finalFromServer = responseEntity.getBody();
+
+        assertThat(finalFromServer).isNotEmpty();
+        assertThat(finalFromServer.length).isEqualTo(1);
+        assertThat(finalFromServer[0]).isEqualToComparingFieldByField(modified);
     }
 
 }
