6a8d9f7bf376f014f9d69c8dc0bdd505a311a71f
tiyb <thinkinginsideyourbox@gmail.com>
2021-03-18 16:14:40 -0400

    updated tests to add REST tests

--- src/test/java/com/tiyb/tev/controller/TevStagingMultiBlog.java
+++ src/test/java/com/tiyb/tev/controller/TevStagingMultiBlog.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-import com.tiyb.tev.TevTestingClass;
+import com.tiyb.tev.datamodel.staging.StagingPost;
+import com.tiyb.tev.html.HtmlTestingClass;
 
 /**
  * Tests for staging posts, ensuring that they are separated by blog
@@ -16,10 +19,12 @@ import com.tiyb.tev.TevTestingClass;
  * @author tiyb
  *
  */
-public class TevStagingMultiBlog extends TevTestingClass {
+public class TevStagingMultiBlog extends HtmlTestingClass {
 
     @Autowired
     private TEVStagingController controller;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     /**
      * Clean out all staged posts before each test
@@ -51,6 +56,33 @@ public class TevStagingMultiBlog extends TevTestingClass {
     }
 
     /**
+     * Creation of staged posts across multiple blogs, via REST
+     */
+    @Test
+    public void multiBlogStagingPostsRest() {
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), MAIN_BLOG_NAME, "1"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "2"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "3"), "",
+                StagingPost.class);
+
+        ResponseEntity<String[]> responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), MAIN_BLOG_NAME), String[].class);
+        String[] posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(1);
+        assertThat(posts[0]).isEqualTo("1");
+        responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), SECOND_BLOG_NAME), String[].class);
+        posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(2);
+        assertThat(posts[0]).isEqualTo("2");
+        assertThat(posts[1]).isEqualTo("3");
+    }
+
+    /**
      * Deletion of a staged post doesn't impact others
      */
     @Test
@@ -72,6 +104,34 @@ public class TevStagingMultiBlog extends TevTestingClass {
     }
 
     /**
+     * Deletion of a staged post doesn't impact others, via REST
+     */
+    @Test
+    public void deleteAStagedostRest() {
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), MAIN_BLOG_NAME, "1"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "2"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "3"), "",
+                StagingPost.class);
+
+        restTemplate.delete(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "2"));
+
+        ResponseEntity<String[]> responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), MAIN_BLOG_NAME), String[].class);
+        String[] posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(1);
+        assertThat(posts[0]).isEqualTo("1");
+        responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), SECOND_BLOG_NAME), String[].class);
+        posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(1);
+        assertThat(posts[0]).isEqualTo("3");
+    }
+
+    /**
      * Deleting all staged posts for a blog doesn't impact other blogs
      */
     @Test
@@ -89,6 +149,33 @@ public class TevStagingMultiBlog extends TevTestingClass {
         posts = controller.getAllPostsForBlog(SECOND_BLOG_NAME);
         assertThat(posts).isNotNull();
         assertThat(posts.size()).isEqualTo(0);
+    }
+
+    /**
+     * Deleting all staged posts for a blog doesn't impact other blogs, via REST
+     */
+    @Test
+    public void deleteAllStagedPostsFromABlogRest() {
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), MAIN_BLOG_NAME, "1"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "2"), "",
+                StagingPost.class);
+        restTemplate.postForObject(String.format("%s/staging-api/posts/%s/%s", baseUri(), SECOND_BLOG_NAME, "3"), "",
+                StagingPost.class);
+
+        restTemplate.delete(String.format("%s/staging-api/posts/%s", baseUri(), SECOND_BLOG_NAME));
+
+        ResponseEntity<String[]> responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), MAIN_BLOG_NAME), String[].class);
+        String[] posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(1);
+        assertThat(posts[0]).isEqualTo("1");
+        responseEntity = restTemplate
+                .getForEntity(String.format("%s/staging-api/posts/%s", baseUri(), SECOND_BLOG_NAME), String[].class);
+        posts = responseEntity.getBody();
+        assertThat(posts).isNotNull();
+        assertThat(posts.length).isEqualTo(0);
     }
 
 }
